8eed81e69e2329c7c48d1699a517ddb32c8f81fc
Solo83 <gstsamara@gmail.com>
2020-10-22 18:15:02 +0400

    New Tasks

---
+++ src/Lesson8/Task0828.java
@@ -0,0 +1,54 @@
+package Lesson8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+Номер месяца
+Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: "May is the 5 month".
+Используйте коллекции.
+
+
+Требования:
+1. Программа должна считывать одно значение с клавиатуры.
+2. Программа должна выводить текст на экран.
+3. Программа должна использовать коллекции.
+4. Программа должна считывать с клавиатуры имя месяца и выводить его номер на экран по заданному шаблону.
+*/
+
+public class Task0828 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "January");
+        map.put(2, "February");
+        map.put(3, "March");
+        map.put(4, "April");
+        map.put(5, "May");
+        map.put(6, "June");
+        map.put(7, "July");
+        map.put(8, "August");
+        map.put(9, "September");
+        map.put(10, "October");
+        map.put(11, "November");
+        map.put(12, "December");
+
+        String month = reader.readLine();
+
+        for (Map.Entry<Integer,String> pair : map.entrySet()) {
+
+            if (pair.getValue().equals(month)) System.out.println(month + " is the "  + pair.getKey() + " month");
+
+        }
+
+
+    }
+}
