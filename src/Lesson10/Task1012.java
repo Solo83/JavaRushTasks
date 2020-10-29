5effde2801d81b9e7d13606c637acc98b600cd4f
Solo83 <gstsamara@gmail.com>
2020-10-29 23:10:51 +0400

    new task

---
+++ src/Lesson10/Task1012.java
@@ -0,0 +1,69 @@
+package Lesson10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+Количество букв
+Введи с клавиатуры 10 строчек и посчитай в них количество различных букв без учета регистра. Результат выведи на экран в алфавитном порядке.
+
+Пример вывода:
+а 5
+б 8
+в 3
+г 7
+д 0
+...
+я 9
+
+
+Требования:
+1. Программа должна 10 раз считывать данные с клавиатуры.
+2. Программа должна выводить текст на экран.
+3. Выведенный текст должен содержать 33 строки.
+4. Каждая строка вывода должна содержать букву русского алфавита, пробел и количество раз, которое буква встречалась в введенных строках.
+*/
+
+public class Task1012 {
+    public static void main(String[] args) throws Exception {
+        TreeMap <Character,Integer> map = new TreeMap<>();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // Алфавит
+        List<Character> alphabet = Arrays.asList(
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж',
+                'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+                'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+                'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+
+
+        // Ввод строк
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            String line = reader.readLine();
+            list.add(line.toLowerCase());
+        }
+
+        for (Character character : alphabet) {
+            int count = 0;
+            for (String s : list) {
+                char[] a = s.toCharArray();
+                for (char c : a) {
+                    if (character.equals(c)) {
+                        count++;
+                    }
+                }
+            }
+
+            map.put(character, count);
+
+        }
+
+        for (Character ch : alphabet) {
+            System.out.println(ch + " " + map.get(ch));
+        }
+
+    }}
+
